bb85fc0312b8cc3ae880e7c6ee49d2b29caff04d
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 02:35:45 +0530

    sorry bhaiya

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Create the Resource Group
 resource "azurerm_resource_group" "this" {
-  name     = "${lower(replace(var.company," ","-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-rg"
+  name     = "${lower(replace(var.company, " ", "-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-rg"
   location = var.location
 
   tags = {
@@ -11,11 +11,11 @@ resource "azurerm_resource_group" "this" {
 
 # Create the Databricks Workspace
 resource "azurerm_databricks_workspace" "this" {
-  name                        = "${lower(replace(var.company," ","-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-workspace"
+  name                        = "${lower(replace(var.company, " ", "-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-workspace"
   resource_group_name         = azurerm_resource_group.this.name
   location                    = azurerm_resource_group.this.location
   sku                         = "trial" // options: Standard, Premium, Trial
-  managed_resource_group_name = "${lower(replace(var.company," ","-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-workspace-rg"
+  managed_resource_group_name = "${lower(replace(var.company, " ", "-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-workspace-rg"
 
   tags = {
     application = var.app_name
