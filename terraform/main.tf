ac0c6bb5295ad521a46e87a32598d4df29bb42cb
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 04:02:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,22 @@
 # Create the Resource Group
 resource "azurerm_resource_group" "this" {
-  name     = "${lower(replace(var.company, " ", "-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-rg"
-  location = var.location
+  name     = "helooo-rg"
+  location = "East US2"
 
   tags = {
-    application = var.app_name
-    environment = var.environment
+    environment = "dev"
   }
 }
 
 # Create the Databricks Workspace
 resource "azurerm_databricks_workspace" "this" {
-  name                        = "${lower(replace(var.company, " ", "-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-workspace"
+  name                        = "helooo-workspace"
   resource_group_name         = azurerm_resource_group.this.name
   location                    = azurerm_resource_group.this.location
   sku                         = "trial" // options: Standard, Premium, Trial
-  managed_resource_group_name = "${lower(replace(var.company, " ", "-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-workspace-rg"
+  managed_resource_group_name = "helooo-workspace-rg"
 
   tags = {
-    application = var.app_name
-    environment = var.environment
+    environment = "dev"
   }
 }
