d5d7655fd59a0a1a33bef6b16be87d85dcc8dae4
vicky <vickyvikneesh6@gmail.com>
2024-03-19 01:35:13 +0530

    solla mudiyathu poda daiii sonna mattu yenna pudnga pora

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,24 @@
-resource "azurerm_databricks_workspace" "intact_databricks_workspace" {
-  name = var.workspace_name
+# Create the Resource Group
+resource "azurerm_resource_group" "this" {
+  name     = "${lower(replace(var.company," ","-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-rg"
   location = var.location
-  resource_group_name = var.resource_group_name
-  sku = var.sku
+
   tags = {
-
-    onwer = var.onwer
-    Environment = var.environment_tag
+    application = var.app_name
+    environment = var.environment
   }
+}
 
+# Create the Databricks Workspace
+resource "azurerm_databricks_workspace" "this" {
+  name                        = "${lower(replace(var.company," ","-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-workspace"
+  resource_group_name         = azurerm_resource_group.this.name
+  location                    = azurerm_resource_group.this.location
+  sku                         = "trial" // options: Standard, Premium, Trial
+  managed_resource_group_name = "${lower(replace(var.company," ","-"))}-${var.app_name}-${var.environment}-${var.shortlocation}-workspace-rg"
+
+  tags = {
+    application = var.app_name
+    environment = var.environment
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,180 +1,60 @@
-variable "workspace_name" {
+# ------------------------
+# Authentication Variables
+# ------------------------
+
+variable "aad_tenant_id" {
   type        = string
-  description = "The name of the Databricks workspace."
+  description = "The id of the Azure Tenant to which all subscriptions belong"
 }
 
+variable "aad_subscription_id" {
+  type        = string
+  description = "The id of the Azure Subscription"
+}
+
+variable "aad_client_id" {
+  type        = string
+  description = "The client id of the Service Principal for interacting with Azure resources"
+}
+
+variable "aad_client_secret" {
+  type        = string
+  description = "The client secret of the Service Principal for interacting with Azure resources"
+  sensitive   = true
+}
+
+# ---------------------
+# Application Variables
+# ---------------------
+
+# Company name 
+variable "company" {
+  type        = string
+  description = "This variable defines the company name used to build resources"
+}
+
+# Application name 
+variable "app_name" {
+  type        = string
+  description = "This variable defines the application name used to build resources"
+}
+
+# Environment
+variable "environment" {
+  type        = string
+  description = "This variable defines the environment to be built"
+}
+
+# Azure region
 variable "location" {
   type        = string
-  description = "The location where the Databricks workspace will be created."
+  description = "Azure region where the resource group will be created"
+  default     = "west europe"
 }
 
-variable "subscription_id" {
+# Azure short region
+variable "shortlocation" {
   type        = string
-  description = "The subscription ID where the Databricks workspace will be created."
+  description = "Azure region where the resource group will be created"
+  default     = "we"
 }
-
-variable "tenant_id" {
-  type        = string
-  description = "The tenant ID associated with the Azure Active Directory where the Databricks workspace will be created."
-}
-
-
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the Azure resource group where the Databricks workspace will be created."
-}
-
-variable "sku" {
-  type        = string
-  description = "The SKU (pricing tier) of the Databricks workspace."
-}
-
-variable "environment_tag" {
-  type        = string
-  description = "The environment tag to apply to the Databricks workspace."
-}
-
-variable "onwer" {
-  type        = string
-  description = "The owner of the Databricks workspace."
-}
-
-# cluster  creation
-
-variable "host" {
-  type        = string
-  description = "The host URL of the Databricks cluster."
-  default     = "https://adb-302867268976868.8.azuredatabricks.net"
-}
-
-variable "token" {
-  type        = string
-  description = "The token used to authenticate with the Databricks cluster."
-  default     = "dapi905df3d8faa2f35e2b9242782c74b4a8-3"
-}
-
-variable "cluster_name" {
-  type        = string
-  description = "The name of the Databricks cluster."
-  default     = "intact_cluster"
-}
-
-variable "spark_version" {
-  type        = string
-  description = "The version of Apache Spark used on the Databricks cluster."
-  default     = "7.3.x-scala2.12"
-}
-
-variable "autotermination_minutes" {
-  type        = number
-  description = "The number of minutes before the cluster is automatically terminated when no jobs are running."
-  default     = 20
-}
-
-variable "min_workers" {
-  type        = number
-  description = "The minimum number of worker nodes for autoscaling."
-  default     = 2
-}
-
-variable "max_workers" {
-  type        = number
-  description = "The maximum number of worker nodes for autoscaling."
-  default     = 50
-}
-
-# variable "availability" {
-#   type        = string
-#   description = "The availability type of the cluster."
-#   default     = "SPOT_WITH_FALLBACK_AZURE"
-# }
-
-# variable "first_on_demand" {
-#   type        = number
-#   description = "The number of on-demand instances to start when the cluster is created."
-#   default     = 1
-# }
-
-# variable "spot_bid_max_price" {
-#   type        = number
-#   description = "The maximum price you are willing to pay for Spot Instances."
-#   default     = 100
-# }
-
-# variable "ResourceClass" {
-#   type        = string
-#   description = "The resource class of the Databricks cluster."
-#   default     = "SingleNode"
-# }
-
-# variable "driver_node_type_id" {
-#   type        = string
-#   description = "The ID of the driver node type for the cluster."
-#   default     = ""
-# }
-
-# variable "instance_pool_id" {
-#   type        = string
-#   description = "The ID of the instance pool to use for the cluster."
-#   default     = ""
-# }
-
-# variable "driver_instance_pool_id" {
-#   type        = string
-#   description = "The ID of the instance pool for the driver nodes of the cluster."
-#   default     = ""
-# }
-
-# variable "policy_id" {
-#   type        = string
-#   description = "The ID of the policy to be applied to the cluster."
-#   default     = null
-# }
-
-# variable "single_user_name" {
-#   type        = string
-#   description = "The username for the single user of the cluster."
-#   default     = ""
-# }
-
-# variable "idempotency_token" {
-#   type        = string
-#   description = "The token used to ensure idempotent updates."
-#   default     = ""
-# }
-
-# variable "ssh_public_keys" {
-#   type    = list(string)
-#   description = "Public SSH keys for accessing the cluster."
-#    default = ["public_key_1", "public_key_2"]
-# }
-
-# variable "DD_API_KEY" {
-#   type        = string
-#   description = "API key for Datadog."
-# }
-
-# variable "DD_ENV" {
-#   type        = string
-#   description = "Environment for Datadog."
-#   default     = "production"
-# }
-
-
-# variable "spark_conf" {
-#   type        = map(any)
-#   description = "Additional Spark configurations for the cluster."
-#   default     = {
-#     "spark.databricks.io.cache.enabled"      = true
-#     "spark.databricks.io.cache.maxDiskUsage" = "50g"
-#     "spark.databricks.io.cache.maxMetaDataCache" = "1g"
-#     "spark.databricks.repl.allowedLanguages" = "python,sql"
-#   }
-# }
-
-
-
-
-
-
-
