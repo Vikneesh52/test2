0471ecedca0ff002f736a5ac4d4bcb814629df12
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 03:19:31 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,9 +7,9 @@ terraform {
   }
 }
 provider "azurerm" {
-  client_id       = var.aad_client_id
-  client_secret   = var.aad_client_secret
-  tenant_id       = var.aad_tenant_id
-  subscription_id = var.aad_subscription_id
+  #client_id       = var.aad_client_id
+  #client_secret   = var.aad_client_secret
+  #tenant_id       = var.aad_tenant_id
+  #subscription_id = var.aad_subscription_id
   features {}
 }
