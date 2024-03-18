198bd918dc3301aac29c440c4aee12283e39662f
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 04:05:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,8 @@ resource "azurerm_databricks_workspace" "this" {
   name                        = "helooo-workspace"
   resource_group_name         = azurerm_resource_group.this.name
   location                    = azurerm_resource_group.this.location
-  sku                         = "trial" // options: Standard, Premium, Trial
-  managed_resource_group_name = "helooo-workspace-rg"
+  sku                         = "trial" // Options: Standard, Premium, Trial
+  managed_resource_group_name = azurerm_resource_group.this.name // Use the same resource group as the workspace for managed resources
 
   tags = {
     environment = "dev"
