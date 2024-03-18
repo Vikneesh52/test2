b20c6a1ef15c1ff57b4c0f67bdd8aaa4c7661018
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 03:35:13 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,3 @@
-# ------------------------
-# Authentication Variables
-# ------------------------
-
-variable "aad_tenant_id" {
-  type        = string
-  description = "The id of the Azure Tenant to which all subscriptions belong"
-}
-
-variable "aad_subscription_id" {
-  type        = string
-  description = "The id of the Azure Subscription"
-}
-
-variable "aad_client_id" {
-  type        = string
-  description = "The client id of the Service Principal for interacting with Azure resources"
-}
-
-variable "aad_client_secret" {
-  type        = string
-  description = "The client secret of the Service Principal for interacting with Azure resources"
-  sensitive   = true
-}
-
 # ---------------------
 # Application Variables
 # ---------------------
