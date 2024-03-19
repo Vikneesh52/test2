638163abe0374a70af95023ca72b407554f54045
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 05:51:13 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,6 +12,6 @@ terraform {
 provider "azurerm" {
   features {}
 }
-provider "databricks" {
-  azure_workspace_resource_id = azurerm_databricks_workspace.this.id
+provider "azurerm_databricks" {
+  features {}
 }
