a8bb43ffbfa66b609e090255c0b487d8103d4018
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 07:56:49 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,6 +13,6 @@ provider "azurerm" {
   features {}
 }
 provider "databricks" {
-  azure_workspace_resource_id = "638868633463028"
+  azure_workspace_resource_id = "/subscriptions/31324520-9b0f-447e-98f4-c8ac3667708d/resourceGroups/heloootest-rg/providers/Microsoft.Databricks/workspaces/heloootest-workspace"
   host                        = "adb-638868633463028.8.azuredatabricks.net"
 }
