e2d25284efc9f5da74254e0534a46e4b69acf518
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 08:19:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@
     resource "databricks_cluster" "this" {
     cluster_name            =  "heloootest-data"
     spark_version           = "7.3.x-scala2.12"
-    node_type_id            = "Standard_DS3_v2"
+    node_type_id            = "Standard_DS3_v3"
     num_workers             = 1  # Set num_workers to 1 for a single-node cluster
     autotermination_minutes = 20
     autoscale {
