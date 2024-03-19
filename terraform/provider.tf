664015934fe1baae509fb5d6e3751b244b12c5ee
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 07:11:46 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,4 +13,6 @@ provider "azurerm" {
   features {}
 }
 provider "databricks" {
+  host          = "https://adb-638868633463028.8.azuredatabricks.net"
+  token         = "dapidd2cf69bf1603e0f13f45df519d17c8d-3"
 }
