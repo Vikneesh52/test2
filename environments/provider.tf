95b700d30fab9c25049302046e125ef23980cd2e
vicky <vickyvikneesh6@gmail.com>
2024-03-19 19:55:11 +0530

    Updated accor. req. frmt

--- environments/provider.tf
+++ environments/provider.tf
@@ -11,9 +11,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+#  client_id       = var.aad_client_id
+#  client_secret   = var.aad_client_secret
   tenant_id       = var.aad_tenant_id
   subscription_id = var.aad_subscription_id
-  use_cli = false
 }
 
 provider "databricks" {
