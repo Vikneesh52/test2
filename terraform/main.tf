fadc1c60547dd0ba0c61c9b4874f0543ad6fdb1d
Vikneesh <65058468+Vikneesh52@users.noreply.github.com>
2024-03-19 07:10:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,40 +1,35 @@
-# Create the Resource Group
-resource "azurerm_resource_group" "this" {
-  name     = "heloootest-rg"
-  location = "East US2"
-
-  tags = {
-    environment = "dev"
-  }
-}
-
-# Create the Databricks Workspace
-resource "azurerm_databricks_workspace" "this" {
-  name                        = "heloootest-workspace"
-  resource_group_name         = azurerm_resource_group.this.name
-  location                    = azurerm_resource_group.this.location
-  sku                         = "premium" // Options: Standard, Premium, Trial
-  #managed_resource_group_name = azurerm_resource_group.this.name // Use the same resource group as the workspace for managed resources
-
-  tags = {
-    environment = "dev"
-  }
-}
-
-resource "databricks_cluster" "this" {
-  cluster_name            =  "heloootest-data"
-  spark_version           = "14.1-scala2.12"
-  node_type_id            = "Standard_DS3_v2"
-  num_workers             = 1  # Set num_workers to 1 for a single-node cluster
-  autotermination_minutes = 20
-  autoscale {
+## Create the Resource Group
+#  resource "azurerm_resource_group" "this" {
+#    name     = "heloootest-rg"
+#    location = "East US2"
+#    
+#    tags = {
+#    environment = "dev"
+#    }
+#}
+#  
+#  # Create the Databricks Workspace
+#    resource "azurerm_databricks_workspace" "this" {
+#    name                        = "heloootest-workspace"
+#    resource_group_name         = azurerm_resource_group.this.name
+#    location                    = azurerm_resource_group.this.location
+#  sku                         = "premium" // Options: Standard, Premium, Trial
+#  #managed_resource_group_name = azurerm_resource_group.this.name // Use the same resource group as the workspace for managed resources
+#    
+#    tags = {
+#    environment = "dev"
+#}
+#}
+    
+    resource "databricks_cluster" "this" {
+    cluster_name            =  "heloootest-data"
+    spark_version           = "14.1-scala2.12"
+    node_type_id            = "Standard_DS3_v2"
+    num_workers             = 1  # Set num_workers to 1 for a single-node cluster
+    autotermination_minutes = 20
+    autoscale {
     min_workers = 1  # Set min_workers to 1 for a single-node cluster
     max_workers = 1  # Set max_workers to 1 for a single-node cluster
-  }
-  
-  aad_client_id          = "99349683-b6fe-40ca-9d83-1c9e4bc08aa0"
-  aad_client_secret      = "QFq8Q~sk1gQkOUvRfGTyo.u2eDYRsHqozj_SodfU"
-  aad_tenant_id          = "29dfe2bd-dc40-4934-8c75-8419c7ab1e30"
-  aad_subscription_id    = "31324520-9b0f-447e-98f4-c8ac3667708d"
+}
 }
 
