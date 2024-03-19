dd90703c52710a3f2ec096acdfd97ca0f35c475d
vicky <vickyvikneesh6@gmail.com>
2024-03-19 17:44:00 +0530

    Updated accor. req. frmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -41,10 +41,21 @@ variable "host" {
 
 variable "aad_tenant_id" {
   type        = string
-  description = "This variable defines the tenant id"
+  description = "The id of the Azure Tenant to which all subscriptions belong"
 }
 
 variable "aad_subscription_id" {
   type        = string
-  description = "This variable defines the subscription id"
+  description = "The id of the Azure Subscription"
+}
+
+variable "aad_client_id" {
+  type        = string
+  description = "The client id of the Service Principal for interacting with Azure resources"
+}
+
+variable "aad_client_secret" {
+  type        = string
+  description = "The client secret of the Service Principal for interacting with Azure resources"
+  sensitive   = true
 }
