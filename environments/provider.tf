c1717adf3c3faf2348a4f9db152010b18f42efaf
vicky <vickyvikneesh6@gmail.com>
2024-03-19 19:56:26 +0530

    Updated accor. req. frmt

--- environments/provider.tf
+++ environments/provider.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-#  client_id       = var.aad_client_id
-#  client_secret   = var.aad_client_secret
+  client_id       = var.aad_client_id
+  client_secret   = var.aad_client_secret
   tenant_id       = var.aad_tenant_id
   subscription_id = var.aad_subscription_id
 }
