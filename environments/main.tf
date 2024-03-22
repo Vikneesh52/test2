62bd75e7e7f9fac944acf060579e8e4752f317f0
vicky <vickyvikneesh6@gmail.com>
2024-03-22 09:54:09 +0530

    main.tf for cluster

--- environments/main.tf
+++ environments/main.tf
@@ -20,14 +20,22 @@ resource "azurerm_databricks_workspace" "this" {
   }
 }
 
+# Create the cluster with the "smallest" amount
+# of resources allowed.
+data "databricks_node_type" "smallest" {
+  local_disk = true
+}
+
+# Use the latest Databricks Runtime
+# Long Term Support (LTS) version.
+data "databricks_spark_version" "latest_lts" {
+  long_term_support = true
+}
+
 resource "databricks_cluster" "this" {
-  cluster_name               = "example-cluste"
-#  resource_group_name        = azurerm_resource_group.this.name
-#  location                   = azurerm_resource_group.this.location
-  num_workers                = 2
-  node_type_id               = "Standard_DS3_v2"
-  spark_version              = "7.3.x-scala2.12"
-#  spark_conf {
-#    spark.speculation = "false"
-#  }
+  cluster_name            = var.cluster_name
+  node_type_id            = data.databricks_node_type.smallest.id
+  spark_version           = data.databricks_spark_version.latest_lts.id
+  autotermination_minutes = var.cluster_autotermination_minutes
+  num_workers             = var.cluster_num_workers
 }
--- environments/variables.tf
+++ environments/variables.tf
@@ -80,3 +80,22 @@ variable "host" {
 #  default     = "adb-638868633463028.8.azuredatabricks.net"
   default = ""
 }
+
+
+variable "cluster_name" {
+  description = "A name for the cluster."
+  type        = string
+  default     = "My Cluster"
+}
+
+variable "cluster_autotermination_minutes" {
+  description = "How many minutes before automatically terminating due to inactivity."
+  type        = number
+  default     = 60
+}
+
+variable "cluster_num_workers" {
+  description = "The number of workers."
+  type        = number
+  default     = 1
+}
